55bdf35304235c24e92761933fe66b535c2798a1
OPTIMUS90 <45073070+OPTIMUS90@users.noreply.github.com>
2019-10-23 17:13:33 +0530

    Update DemoApplication.java

--- src/main/java/com/pawan/microservice/demo/DemoApplication.java
+++ src/main/java/com/pawan/microservice/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class DemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
+		System.out.println("calling main method");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
